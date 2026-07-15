2fc8fbaeda8391a70751fa451dc0959f1235bd09
PadraicM71 <72097198+PadraicM71@users.noreply.github.com>
2026-07-15 22:09:23 +0100

    Refine image resizing comments and add geo details
    Updated comments on image resizing and added latitude and longitude to image details.

--- app.py
+++ app.py
@@ -39,21 +39,27 @@ def upload():
 
     # Resize so the longest side is 1500 pixels
     # img.thumbnail((1500, 1500)) # this work fine - but lets try 800
-    # 1000 worked well - going to 800 the reg was still perfect but the other insignificant text
-    # in an image started to suffer - which is fine.
-    img.thumbnail((800, 800)) # 
+    # 1000 worked well - going to 800 the reg was still perfect but the other insignificant text of
+    # an image started to suffer - which is fine - my testing suggests 800 is the perfect balance point
+    # update: on your mac 800 looks like its near the minimum - but on render I think you could go lower!
+    # (explore this!)
+    img.thumbnail((800, 800)) # keep 800
     img.save(filepath)
 
     # Get file details
+
     file_size_bytes = os.path.getsize(filepath)
     file_size_mb = round(file_size_bytes / (1024 * 1024), 2)
+    latitude = request.form.get("latitude")
+    longitude = request.form.get("longitude")
     image_details["filename"] = photo.filename
     image_details["filesize"] = file_size_mb
     image_details["width"] = img.width
     image_details["height"] = img.height
     image_details["format"] = img.format
     image_details["mode"] = img.mode
-
+    image_details["latitude"] = latitude
+    image_details["longitude"] = longitude
 
 
     # *********** START OCR Skunk Works ***********
@@ -67,9 +73,8 @@ def upload():
         for text in line["rec_texts"]:
             print(text)
             ocr_found.append(text)
-            image_details["ocr"]=ocr_found
 
-
+    image_details["ocr"]=ocr_found
 
 
     # *********** FIN OCR Skunk Works *************
