c186d8d34581b54ebc6b288959988cdce8d7796a
PadraicM71 <72097198+PadraicM71@users.noreply.github.com>
2026-07-15 13:14:00 +0100

    Integrate PaddleOCR for image text recognition
    Added PaddleOCR integration to process uploaded images and extract text.

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request, send_file
 from PIL import Image
 import os
 
+from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+    lang="en",
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False,
+    use_textline_orientation=False
+)
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = "uploads"
@@ -11,18 +20,17 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 image_details = {}
 
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+
 @app.route("/upload", methods=["POST"])
 def upload():
-
     photo = request.files["photo"]
-
     filepath = os.path.join(UPLOAD_FOLDER, "latest.jpg")
-
     photo.save(filepath)
 
     # Get file details
@@ -31,19 +39,37 @@ def upload():
 
     # Open image and inspect
     img = Image.open(filepath)
-
     image_details["filename"] = photo.filename
     image_details["filesize"] = file_size_mb
     image_details["width"] = img.width
     image_details["height"] = img.height
     image_details["format"] = img.format
     image_details["mode"] = img.mode
+
+
+
+    # *********** START OCR Skunk Works ***********
+    # do OCR experimenting here and just return the result in 'image_details' for now
+    # late you will make this a function on its own and a specific interface in index.html
+    result = ocr.predict(filepath)
+    # image_details["ocr"] = result
+    print(type(result))
+    for line in result:
+        for text in line["rec_texts"]:
+            print(text)
+            image_details["ocr"]=text
+
+
+
+
+    # *********** FIN OCR Skunk Works *************
     
     return render_template(
         "index.html",
         image=True,
         details=image_details
     )
+
 
 
 @app.route("/image")
@@ -53,6 +79,10 @@ def image():
         mimetype="image/jpeg"
     )
 
+
+
+
+# Paste above from vscode
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
