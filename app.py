ec3beac6808fb3a2544a015d52d60fc428fee7a1
PadraicM71 <72097198+PadraicM71@users.noreply.github.com>
2026-08-03 19:51:42 +0100

    Add jsonify import to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, jsonify
 from PIL import Image, ImageOps
 import os
 import re
