52092d26fbfff4e04473cd8ee969b787400cc96d
PadraicM71 <72097198+PadraicM71@users.noreply.github.com>
2026-08-08 22:27:35 +0100

    Implement get_details function for vehicle registration
    Added function to retrieve vehicle details from a remote server.

--- server_reg.py
+++ server_reg.py
@@ -1,1 +1,26 @@
+# this will be used to develope the code to get Render to call the vehicle_reg function on my Ubuntu server
+# will start with calling it from vscode on my Mac!
+# the reg.py script was developed in another folder webappbasic/webapp2 and put on ubuntu but develope the
+# code to execute it on a server here
+
+import requests
+
+def get_details(reg):
+    response = requests.get(
+        # "http://192.168.1.41:5000/run",       # local home network
+        "https://reg.moranai.net/run",         # over the net!
+        params={"reg": reg},
+        timeout=13
+    )
+
+    result = response.json()
+    return result
+
+
+
+
+if __name__ == '__main__':
+    # for testing:
+    print(get_details("232d1880"))
+
 
