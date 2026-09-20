91f4f2643cb726fade0154473f25487dbfca064b
PadraicM71 <72097198+PadraicM71@users.noreply.github.com>
2026-09-20 17:27:09 +0100

    Refactor get_details to use vehicle lookup API
    Updated the get_details function to use a new API for vehicle lookup and modified the payload structure accordingly.

--- server_reg.py
+++ server_reg.py
@@ -3,24 +3,73 @@
 # the reg.py script was developed in another folder webappbasic/webapp2 and put on ubuntu but develope the
 # code to execute it on a server here
 
+
+
+
+
+
+# OLD **********************************************************************
+
+# import requests
+
+# def get_details(reg):
+#     response = requests.get(
+#         # "http://192.168.1.41:5000/run",       # local home network
+#         "https://reg.moranai.net/run",         # over the net!
+#         params={"reg": reg},
+#         timeout=13
+#     )
+
+#     result = response.json()
+#     return result
+
+
+
+
+# if __name__ == '__main__':
+#     # for testing:
+#     print(get_details("232d1880"))
+
+
+# OLD **********************************************************************
+
+
 import requests
 
+
 def get_details(reg):
-    response = requests.get(
-        # "http://192.168.1.41:5000/run",       # local home network
-        "https://reg.moranai.net/run",         # over the net!
-        params={"reg": reg},
-        timeout=13
+    url = "https://www.vehicleservices.gov.ie/api/v1/public-cmv/cmv/vehicles/lookup"
+
+    payload = {
+        "registrationNumber": reg.upper()
+    }
+
+    response = requests.post(
+        url,
+        json=payload,
+        timeout=30
     )
 
-    result = response.json()
-    return result
+    response.raise_for_status()
+
+    data = response.json()
+
+    vehicle = data.get("vehicleDetails", {})
+    motor_tax = data.get("motorTaxDetails", {})
+    nct = data.get("nctInformation", {})
+
+    return {
+        "Registration": vehicle.get("vehicleRegistrationNumber"),
+        "Make": vehicle.get("vehicleMake"),
+        "Model": vehicle.get("vehicleModel"),
+        "Colour": vehicle.get("vehicleColourEn"),
+        "Current Annual Motor Tax Rate": motor_tax.get("annualMotorRate"),
+        "Motor Tax Expiry": motor_tax.get("motorTaxExpiryDate"),
+        "NCT Expiry": nct.get("nctExpiryDate"),
+        "Motor Tax Status": motor_tax.get("statusEn"),
+        "NCT Status": nct.get("nctStatusEn")
+    }
 
 
-
-
-if __name__ == '__main__':
-    # for testing:
-    print(get_details("232d1880"))
-
-
+if __name__ == "__main__":
+    print(get_details("191D11886"))
